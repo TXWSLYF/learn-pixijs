f04b7be0102811dd7bfe6affbdbb4a220472627c
alex <1060319328@qq.com>
2021-08-10 18:57:02 +0800

    07-graphic-primitives

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // import "./03-tileset";
 // import "./04-texture-atlas";
 // import "./05-move-sprites";
-import "./06-grouping-sprites";
+// import "./06-grouping-sprites";
+import "./07-graphic-primitives";
